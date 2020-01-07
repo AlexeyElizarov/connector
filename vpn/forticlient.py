2cc935f7fafa3ae05b942a558ea9ab1f91c05676
Alexey Elizarov <haakee@gmail.com>
2020-01-07 12:06:40 +0300

    rfix: forticlient minor fixes

--- vpn/forticlient.py
+++ vpn/forticlient.py
@@ -20,9 +20,9 @@ class FortiClient(VPN):
     """
 
     _default_driver = r'C:\Program Files\Fortinet\FortiClient\FortiClient.exe'
+    _is_connected = False
 
     def __init__(self):
-        self._is_connected = False
         self._chrome_options = Options()
         self._chrome_options.binary_location = self._default_driver
         self._chrome_options.add_argument('--remote-debugging-port=12345')
