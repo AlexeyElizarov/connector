da4b83dd0b5afec7efe6fdabf0a04a007ef47183
Alexey Elizarov <haakee@gmail.com>
2020-02-01 10:12:18 +0300

    refact: gui moved to widgets

--- connector/__init__.py
+++ connector/__init__.py
@@ -1,5 +1,5 @@
 from connector import widgets
-from connector.gui import ConnectorGUI as GUI
+from connector.widgets.gui import ConnectorGUI as GUI
 from connector.controllers.base import Connector as Base
 from connector.controllers.saplogon import SAPLogon as SAP
 from connector.controllers.rsabase import RSA
